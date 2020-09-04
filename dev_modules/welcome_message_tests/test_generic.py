cea224a5e33ade97c3d32cbc415d8aa3edecde12
aegroto <aegroto@protonmail.com>
2020-09-04 22:58:03 +0200

    better pytest integration

--- dev_modules/welcome_message_tests/test_generic.py
+++ dev_modules/welcome_message_tests/test_generic.py
@@ -1,32 +1,28 @@
+import pytest
+
 from modules.pytg.development import add_reroute_rule
 
 from modules.pytg.load import manager
 
 from telegram import Update
 
-# Helper functions
-
-def mockbot_test(setup, clear):
-    def inner(func):
-        setup()
-        func()
-        clear()
-
-    return inner
-
 def setup():
-    # add_reroute_rule("bot", "mockbot")
-    print("setup")
+    pass
 
-def clear():
-    print("clear")
+def cleanup():
+    pass
+
+@pytest.fixture(autouse=True)
+def test_container():
+    setup()
+    yield
+    cleanup()
 
 def __load_test_update(name, bot):
     resources_manager = manager("resources")
     return Update.de_json(resources_manager.load_resource("welcome_message_tests", name, path="test_updates", loader="json"), bot)
 
 # Tests
-@mockbot_test(setup = setup, clear = clear)
 def test_simple_welcome():
     mockbot_manager = manager("mockbot")
     bot = mockbot_manager.bot
@@ -43,7 +39,7 @@ def test_simple_welcome():
 
     mockbot_manager.add_trigger("POST", "sendMessage",
         {
-            "{'chat_id': -436279640, 'text': 'Welcome to my group, Chat ID Echo!', 'disable_notification': False}": success_guard
+            str({'chat_id': -436279640, 'text': 'Welcome to my group, Chat ID Echo!', 'disable_notification': False}): success_guard
         }
     )
 
--- launch_tests.py
+++ launch_tests.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
 
     launch(dev_mode = True)
 
-    pytest.main()
+    pytest.main(["--cov=modules"])
 
     sys.exit()
