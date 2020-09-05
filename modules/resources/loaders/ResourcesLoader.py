f7e38de3088380f2131e5ed7cffe5a4908e133d7
aegroto <aegroto@protonmail.com>
2020-09-05 11:58:59 +0200

    modules cleared

--- modules/resources/loaders/ResourcesLoader.py
+++
@@ -1,6 +0,0 @@
-class ResourcesLoader():
-    def load_resource(self, module, resource_id, path=""):
-        raise NotImplementedError
-
-    def save_resource(self, module, resource_id, data, path=""):
-        raise NotImplementedError
