bf665e5c98094324f22f624b8bd1239210397196
aegroto <aegroto@protonmail.com>
2021-08-01 21:37:19 +0200

    updated main

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-import telegram, logging, threading, datetime, argparse, sys
+import logging, os
 
-from telegram.ext import Updater, MessageHandler, CommandHandler, Filters 
-
-from modules.pytg.init import boot, initialize, launch 
+from modbo.init import boot, initialize, launch
 
 def __main():
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO,
-        # filename="pytg-bot.log"
+        level=logging.INFO
     )
 
-    logging.info(" ### Launching Bot... ### ")
-    logging.info(str(datetime.datetime.now()))
-
-    parser = argparse.ArgumentParser(description='PyTG command line launcher')
-    parser.add_argument("--main-module")
-    parser.add_argument("--devmode", action="store_true")
-
-    args = parser.parse_args()
-
-    main_module = args.main_module
-    dev_mode = args.devmode
-    
-    if not main_module:
-        main_module = "bot"
-        
-    boot(dev_mode)
+    boot(False)
     initialize()
-    launch(main_module)
+    launch(os.environ["MAIN_MODULE"])
 
 if __name__ == '__main__':
     __main()
