1f2da9047a3ca5731e5b671f2c3857d6be997fed
aegroto <aegroto@protonmail.com>
2021-08-01 21:25:40 +0200

    removed utils

--- utils/scaffolding.py
+++
@@ -1,9 +0,0 @@
-def get_folders_path(development):
-    if development:
-        modules_folder = "dev_modules"
-        content_folder = "dev_content"
-    else:
-        modules_folder = "modules"
-        content_folder = "content"
-
-    return modules_folder, content_folder
